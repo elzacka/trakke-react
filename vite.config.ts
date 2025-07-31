4e3b67de1372b029577b7a60ab58012f2bb8e3db
elzacka <lene.zachariassen@gmail.com>
2025-08-01 00:36:35 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/trakke-react/', // Dette må matche repository-navnet ditt
+  build: {
+    outDir: 'dist',
+  },
 })
